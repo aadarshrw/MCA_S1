71735c31bca93377225032559370dbc864d82759
Aadarsh Warrier <126311721+aadarshrw@users.noreply.github.com>
2023-11-19 16:04:52 +0530

    Delete PYTHON/28-sumoflistusingfor.py

--- PYTHON/28-sumoflistusingfor.py
+++
@@ -1,9 +0,0 @@
-def sum_list(l):
-    "'Sum of elements of a list'"
-    sum = 0
-    for i in l:
-        sum+=i
-    return sum
-n = input("Enter the list elements: ")
-n = list(map(int,n.split(',')))
-print('sum of elements: ',sum_list(n))
