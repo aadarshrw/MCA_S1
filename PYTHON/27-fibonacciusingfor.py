e3343624d633a8ee36a775125e1320b08d59549e
Aadarsh Warrier <126311721+aadarshrw@users.noreply.github.com>
2023-11-19 16:04:37 +0530

    Delete PYTHON/27-fibonacciusingfor.py

--- PYTHON/27-fibonacciusingfor.py
+++
@@ -1,8 +0,0 @@
-def fibo(n):
-    "'Generate nth Fibonacci number'"
-    a,b = 0,1
-    for i in range(n):
-        a,b = b,a+b
-    return a
-n = int(input("Enter number: "))
-print('%dth fibonacci number is %d'%(n,fibo(n)))
