b00ffb40d794d8bb59e0e878f0982e015c74c972
Aadarsh Warrier <126311721+aadarshrw@users.noreply.github.com>
2023-12-02 12:42:22 +0530

    Update BFS.c

--- C/BFS.c
+++ C/BFS.c
@@ -1,49 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
-int q[10],visited[10],i,j,n,adj[10][10],front=1,rear=0,v,item;
-void insert(int v)
-{
-	rear++;
-	q[rear]=v;
+#include <stdio.h>
+#include <stdlib.h>
+
+int q[10], visited[10], i, j, n, adj[10][10], front = 1, rear = 0, v, item;
+
+void insert(int v) {
+    rear++;
+    q[rear] = v;
 }
-int get()
-{
-	v= q[front];
-	front++;
-	return v;
+
+int get() {
+    v = q[front];
+    front++;
+    return v;
 }
-int main()
-{
-	printf("Total no of vertices :: ");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		visited[i]=0;
-	}
-	printf("\nenter the adjacency matrix!\n");
-	for(i=1;i<=n;i++)
-	{
-		for(j=1;j<=n;j++)
-		{
-			scanf("%d",&adj[i][j]);
-		}
-	}
-	printf("spanning tree edges are:\n");
-	insert(1);
-	for(i=1;i<=n;i++)
-	{
-		item=get();
-		visited[item]=1;
-		for(j=i+1;j<=n;j++)
-		{
-			if(adj[item][j]==1 && visited[j]==0)
-			{
-				visited[j]=1;
-				insert(j);
-				printf("edge(%d,%d)\n",item,j);
-			}
-		}
-	}
-	printf("\n");
-	return 0;
+
+int isEmpty() {
+    return front > rear;
+}
+
+int main() {
+    printf("Total no of vertices :: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("\nEnter the adjacency matrix!\n");
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Spanning tree edges are:\n");
+    insert(1);
+
+    while (!isEmpty()) {
+        item = get();
+        visited[item] = 1;
+
+        for (j = 1; j <= n; j++) {
+            if (adj[item][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                insert(j);
+                printf("edge(%d,%d)\n", item, j);
+            }
+        }
+    }
+
+    printf("\n");
+    return 0;
 }
