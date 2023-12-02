3a67a289e5aee5f6cea2471ee863d2fc5f2b819b
Aadarsh Warrier <126311721+aadarshrw@users.noreply.github.com>
2023-12-02 12:36:37 +0530

    Update DFS.c

--- C/DFS.c
+++ C/DFS.c
@@ -1,46 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
-int s[10],visited[10],i,j,n,adj[10][10],top=0,v,k,item;
-void push(int v)
+
+int s[10], visited[10], i, j, n, adj[10][10], top = 0, v, k, item;
+
+void push(int v) 
 {
-	top++;
-	s[top]=v;
+    top++;
+    s[top] = v;
 }
-int pop()
+
+int pop() 
 {
-	v= s[top];
-	top--;
-	return v;
+    v = s[top];
+    top--;
+    return v;
 }
-int main()
+
+int main() 
 {
-	printf("Total no of vertices :: ");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-		 visited[i]=0;
-	printf("\nenter the adjacency matrix!\n");
-	for(i=1;i<=n;i++)
+    printf("Total no of vertices :: ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= n; i++)
+        visited[i] = 0;
+    
+    printf("\nEnter the adjacency matrix!\n");
+    
+    for (i = 1; i <= n; i++) 
+    {
+        for (j = 1; j <= n; j++) 
 	{
-		for(j=1;j<=n;j++)
-		{
-				scanf("%d",&adj[i][j]);
-		}
-	}
-	printf("spanning tree edges are:\n");
-	push(1);
-	for(i=1;i<=n;i++)
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    
+    printf("Spanning tree edges are:\n");
+    push(1);
+    
+    while (top != 0) 
+    {
+        item = pop();
+        printf("%d-->", item);
+        visited[item] = 1;
+        
+        for (j = 1; j <= n; j++) 
 	{
-		item=pop();
-		printf("%d-->",item);
-		visited[item]=1;
-		for(j=i+1;j<=n;j++)
-		{
-			if(adj[item][j]==1 && visited[j]==0)
-			{
-				visited[j]=1;
-				push(j);
-			}
-		}	
-	}
-	return 0;
+            if (adj[item][j] == 1 && !visited[j]) 
+	    {
+                visited[j] = 1;
+                push(j);
+            }
+        }
+    }
+    
+    return 0;
 }
